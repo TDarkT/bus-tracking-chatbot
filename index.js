36f8d5d39cc516b4c3a110e74d26a51cff49067d
Milo <minhlong1312@gmail.com>
2020-05-25 22:39:04 +0700

    Port fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const BOT_TOKEN = 'EAAsbsQXJVEoBAFZAudwR0Xmnl2aHe9V8QnBXUk2UkkW6btOaq4A9ZA9I8gcjZCXfrZCv9h5KfGRu7CjdQb0v7YXgnbqRBoxiubNu88B4zioKzxu9wFC8lliDqEarxFsoCVNvJ3E2NjkafEmI21BSFDnhGpkN9J7czPQ6uBs8pCKNVcZB4ZAln7s3xe3jGiCuwZD'
-app.listen(8080, () => {
+app.listen(process.env.PORT || 80, () => {
     console.log("Listening on port 8080");
 })
 
