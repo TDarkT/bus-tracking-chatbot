f3b026eb7b6fd06dcdf1db31fb91ed4a656fc14a
Milo <minhlong1312@gmail.com>
2020-05-25 22:28:01 +0700

    Change port to 80 and fixing filepath

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const BOT_TOKEN = 'EAAsbsQXJVEoBAFZAudwR0Xmnl2aHe9V8QnBXUk2UkkW6btOaq4A9ZA9I8gcjZCXfrZCv9h5KfGRu7CjdQb0v7YXgnbqRBoxiubNu88B4zioKzxu9wFC8lliDqEarxFsoCVNvJ3E2NjkafEmI21BSFDnhGpkN9J7czPQ6uBs8pCKNVcZB4ZAln7s3xe3jGiCuwZD'
-app.listen(8080, () => {
-    console.log("Listening on port 8080");
+app.listen(80, () => {
+    console.log("Listening on port 80");
 })
 
-app.get("/", async (req, res) => {
-    res.sendFile("./index.html");
+app.get("/",  (req, res) => {
+    res.sendFile(res.sendFile(__dirname + '/index.html'));
     res.status(200);
 })
